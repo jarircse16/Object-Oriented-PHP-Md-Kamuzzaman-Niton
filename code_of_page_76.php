4d865ba99cad952e4c07d850cc4e6719fcba529b
Jarir Ahmed Deedat <129315010+jarircse16@users.noreply.github.com>
2023-06-22 17:38:31 +0600

    Corrected Version

--- code_of_page_76.php
+++ code_of_page_76.php
@@ -1,28 +1,28 @@
 <?php
 	
 	class Consultant {
-		private $name = " ";
-		private $role = " ";
+		private $name = "";
+		private $role = "";
 		
 		public function __construct($name , $role){
 			$this->name = $name;
 			$this->role = $role;
 		}
-		public function setConsultant()
+		public function setConsultant($name , $role)
 		{
 			$this->name = $name;
 			$this->role = $role;
 		}
 		public function showConsultant (){
-			echo "Consultant: " .$this->name . "\n";
-			echo "Role: " .$this->role . "\n";
+			echo "Consultant: " .$this->name . "<br>";
+			echo "Role: " .$this->role . "<br>";
 		}
 	}
 	
-	$p1 = new Consultant ("Jarir", "PHP Developer");
+	$p1 = new Consultant ("Riasat", "Ace Pilot");
 	$p2 = $p1;
 	
-	$p2 = new Consultant ("Riasat", "Ace Pilot");
+	$p2->setConsultant("Jarir", "PHP Developer");
 	$p1->showConsultant();
 	$p2->showConsultant();
 	?>
