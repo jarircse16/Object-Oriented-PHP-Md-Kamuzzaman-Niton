021bb6f14eee30127076362091def93c2e669fe5
Jarir Ahmed Deedat <129315010+jarircse16@users.noreply.github.com>
2023-06-26 03:49:03 -0700

    Object Styled Definition of a Member

---
+++ code_of_page_107.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$member_info = new stdClass;
+	
+	$member_info->name = "Jarir Ahmed";
+	$member_info->position = "Software Engineer";
+	$member_info->organization = "Aamra Technologies Limited";
+	$member_info->behaviour = "Cool";
+	
+	print_r($member_info);
+
+?>
