fa5218845e3a9b0787cc80bbebe62bdf0de2e256
Jarir Ahmed Deedat <129315010+jarircse16@users.noreply.github.com>
2023-06-26 03:48:33 -0700

    PHP Array Display Elements
    PHP Array Display Elements

---
+++ code_of_page_106.php
@@ -0,0 +1,5 @@
+<?php
+	
+		$member_info_array = array( "name" => "Jarir Ahmed" , "position" => "Software Engineer" , "organization" => "JarirIT" , "behaviour" => "good");
+		print_r($member_info_array);
+		?>
